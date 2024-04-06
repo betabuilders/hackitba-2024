369754b649661174907a8aa0e98c75014c50c905
Tomás Pietravallo <hello@tomaspietravallo.com>
2024-04-06 15:37:04 -0300

    feat(front): improve expense grid

--- front/components/category-badge.tsx
+++ front/components/category-badge.tsx
@@ -1,8 +1,9 @@
 import { CATEGORIES } from "@/lib/constants";
 import { Badge } from "./ui/badge";
 import { cn } from "@/lib/utils";
+import { DOMAttributes, ReactElement, ReactNode } from "react";
 
-export default function CategoryBadge({ category }: { category: string }) {
+export default function CategoryBadge({ category, disabled, className, ...props }: { category: string, className?: string, disabled?: boolean } & {[key in keyof DOMAttributes<HTMLDivElement>]: DOMAttributes<HTMLDivElement>[key]} ) {
     // @ts-ignore
-    return <Badge className={cn("inline-block", CATEGORIES[category].style)}>{CATEGORIES[category].name}</Badge>
+    return <Badge {...props} disabled={disabled} aria-disabled={disabled} className={cn("inline-block", className, CATEGORIES[category].style)}>{CATEGORIES[category].name}</Badge>
 }
--- front/components/expense-grid.tsx
+++ front/components/expense-grid.tsx
@@ -1,39 +1,39 @@
-import { CATEGORIES } from "@/lib/constants";
+import { CATEGORIES, Expense } from "@/lib/constants";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { Badge } from "./ui/badge";
 import CategoryBadge from "./category-badge";
 import { Plus } from "lucide-react";
 
-export type Expense = {
-	id: string,
-    name: string,
-    amount: string,
-    description: string,
-    categories: (keyof typeof CATEGORIES)[],
-}
+export default function ExpenseGrid(props: { expenses: Expense[], role: string, filter: string[] }) {
+	console.log(props.filter);
+	const filteredExpenses = props.expenses.filter(e => props.filter?.every((f) => e.categories.includes(f) ) ?? true ).map((info, i) => {
+		return (
+		  <a href={props.role == 'ADMIN' ? `./expenses/${info.id}` : `./pay/${info.id}/`} className="w-full h-full">
+			  <Card key={i} className="w-full h-full">
+				  <CardHeader>
+				  <CardTitle >{info.name}</CardTitle>
+				  <CardDescription className="flex flex-col gap-2">
+					  AR$ {info.amount}
+				  </CardDescription>
+				  <div className="flex flex-row gap-2">
+					  { ...info.categories.map((e, k) => <CategoryBadge key={k} category={e}/> ) }
+				  </div>
+				  </CardHeader>
+				  <CardContent>
+				  <p>{info.description}</p>
+				  </CardContent>
+			  </Card>
+		  </a>
+		);
+	});
 
-export default function ExpenseGrid(props: { expenses: Expense[], role: string }) {
-    return <div className="grid-cols grid auto-rows-min grid-cols-1 lg:grid-cols-4 items-start justify-start gap-4">
-        {...props.expenses.map((info, i) => {
-          return (
-            <a href={props.role == 'ADMIN' ? `./expenses/${info.id}` : `./pay/${info.id}/`} className="w-full h-full">
-				<Card key={i} className="w-full h-full">
-					<CardHeader>
-					<CardTitle >{info.name}</CardTitle>
-					<CardDescription className="flex flex-col gap-2">
-						AR$ {info.amount}
-					</CardDescription>
-					<div className="flex flex-row gap-2">
-						{ ...info.categories.map((e, k) => <CategoryBadge category={e}/> ) }
-					</div>
-					</CardHeader>
-					<CardContent>
-					<p>{info.description}</p>
-					</CardContent>
-				</Card>
-            </a>
-          );
-        })}
+    return <div className="grid-cols grid auto-rows-min grid-cols-1 lg:grid-cols-4 items-start justify-start gap-4 w-full">
+        {
+			...(filteredExpenses.length == 0 ? [<Card className="p-4 col-span-full">
+				<p className="text-xl">No se encontro un saldo perteneciente a las categorias:</p>
+				<div className="inline-flex w-full gap-4">{ props.filter.map((f, i) => <CategoryBadge key={i} category={f}/>) }</div>
+			</Card>] : filteredExpenses)
+		}
 		
 		{
 			props.role === 'ADMIN' ?
