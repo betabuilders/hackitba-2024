deea3b5194fd898a602edb673d861bdcd3827b9a
Tomás Pietravallo <hello@tomaspietravallo.com>
2024-04-06 04:35:46 -0300

    feat: improved mobile navbar, desktop usePathname styling

--- front/components/navbars.tsx
+++ front/components/navbars.tsx
@@ -19,34 +19,55 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { LINKS } from "@/lib/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export function MobileNavBar({
   children,
   ...props
 }: { children?: ReactNode } & { [key: string]: any }) {
-	return <nav className="block lg:hidden">
-		<div className="absolute rounded border top-4 left-4 p-4 backdrop-blur-md">
-			<HamburgerMenuIcon/>
-		</div>
-	</nav>;
+	const pathname = usePathname();
+
+	return <Sheet>
+		<SheetTrigger asChild>
+				<Button variant="ghost"
+					className="sticky z-50 top-0 m-5 p-6 border text-base bg-slate-200/20 backdrop-blur-md hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden"
+				>
+				<HamburgerMenuIcon/>
+			</Button>
+		</SheetTrigger>
+		<SheetContent side={"left"}>
+			<ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
+				{LINKS.map((link) => 
+					<a href={link.href} key={link.name}>
+						<SheetClose className="block">
+							<p className={`text-xl pb-4 font-medium ${pathname == link.href ? "opacity-50 pointer-events-none" : ""}`}>{link.name}</p>
+						</SheetClose>
+					</a>
+				)}
+			</ScrollArea>
+		</SheetContent>
+	</Sheet>;
 }
 
 export function DesktopNavBar({
   children,
   ...props
 }: { children?: ReactNode } & { [key: string]: any }) {
-  return (
-    <nav className="hidden lg:block">
-      <div className="flex h-16 w-full items-center justify-between bg-blue-300 text-center font-bold px-24">
-        <p>Beta builders</p>
-        <div className="flex flex-row justify-end gap-4 font-semibold">
-			{...LINKS.map((l) => {
-				return <div className="py-1 px-2 rounded-sm border bg-white/40 border-blue-200">
-					<a key={l.name} href={l.href}>{l.name}</a>
-				</div>
-			})}
-        </div>
-      </div>
-    </nav>
-  );
+	const pathname = usePathname();
+	
+	return (
+		<nav className="hidden lg:block">
+		<div className="flex h-16 w-full items-center justify-between bg-blue-300 text-center font-bold px-24">
+			<p>Beta builders</p>
+			<div className="flex flex-row justify-end gap-4 font-semibold">
+				{...LINKS.map((l) => {
+					return <div className={cn("py-1 px-2 rounded-sm border bg-white/40 border-blue-200", pathname == l.href ? "font-normal text-gray-500" : "")}>
+						<a key={l.name} href={l.href}>{l.name}</a>
+					</div>
+				})}
+			</div>
+		</div>
+		</nav>
+	);
 }
