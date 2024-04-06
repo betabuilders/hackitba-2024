73cedb6dfed8127fa239dc30b55da851e3a52e80
Tomás Pietravallo <hello@tomaspietravallo.com>
2024-04-06 14:54:20 -0300

    feat(front): add bank details form

--- front/app/pay/[expense]/page.tsx
+++ front/app/pay/[expense]/page.tsx
@@ -1,10 +1,54 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { EXPENSES } from "@/lib/constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 export default function PayExpense({ params } : { params: { expense: string }} ) {
-    return <main className="flex min-h-screen flex-col items-center justify-start p-4 my-8 lg:p-24 pt-6 gap-8">
-        <h1>
-            Pagar desde saldo: {params.expense}
-        </h1>
-        <Button className="bg-blue-400">Pagar</Button>
+    const formSchema = z.object({
+        cbu: z.string().min(4, {
+            message: "Porfavor ingrese un numero de CBU correcto"
+        })
+    });
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            cbu: "",
+        },
+    });
+
+    return <main className="flex min-h-screen flex-col items-center justify-center p-4 my-8 lg:p-24 pt-6 gap-8">
+        <div className="flex flex-col justify-center items-center h-full gap-8">
+            <h1 className="text-2xl lg:text-3xl uppercase">Desde: {EXPENSES.find((e) => e.id == params.expense)?.name }</h1>
+            <h2>hacia</h2>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(() => {})} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="cbu"
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>CBU</FormLabel>
+                        <FormControl>
+                            <Input className="uppercase" placeholder="Ingrese cbu" type="email" {...field} />
+                        </FormControl>
+                        <FormDescription>Cuenta a la que transferir dinero</FormDescription>
+                        <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" className="w-full bg-blue-200">Submit</Button>
+            </form>
+        </Form>
+        <hr className="w-2/3 my-6"/>
+        <p className="text-sm opacity-50">
+            (Pagar desde saldo ID: {params.expense.slice(params.expense.indexOf.length - 9)})
+        </p>
+        </div>
     </main>
 }
